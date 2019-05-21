7ab7e8c03ca26662dfeb9583bd04f81ba6a8c4db
polleichtnermoritz <moritz.polleichtner@outlook.com>
2019-05-21 11:45:01 +0200

    3/7

--- battleship.cpp
+++ battleship.cpp
@@ -13,3 +13,55 @@
 #include <stdio.h>
 
 #include "battleship.h"
+#define SIZE 10
+static CellContent myField[SIZE][SIZE];
+static CellContent enemyField[SIZE][SIZE];
+static CellContent myGuesses[SIZE][SIZE];
+
+void load_game()
+{
+  FILE *my_fd = fopen("battleship.my", "r+");
+  FILE *op_fd = fopen("battleship.op", "r+");
+
+  for (int i = 0; i < FIELDSIZE ; i++)
+  {
+    for (int j = 0; j < FIELDSIZE; j++)
+    {
+      fread(&myField[i][j], 4, 1, my_fd);
+      fread(&enemyField[i][j], 4, 1, op_fd);
+      myGuesses[i][j] = Unknown;
+    }
+  }
+  fclose(my_fd);
+  fclose(op_fd);
+}
+
+CellContent get_shot(int row, int col)
+{
+  if (row < 0 || row >= FIELDSIZE || col < 0 || col >= FIELDSIZE)
+  {
+    return OutOfRange;
+  }
+  return myField[row][col];
+}
+
+bool shoot(int row, int col)
+{
+  if (row < 0 || row >= FIELDSIZE || col < 0 || col >= FIELDSIZE)
+  {
+    return false;
+  }
+
+
+
+}
+
+CellContent get_my_guess(int row, int col)
+{
+  if (row < 0 || row >= FIELDSIZE || col < 0 || col >= FIELDSIZE)
+  {
+    return OutOfRange;
+  }
+
+  return myGuesses[row][col];
+}
